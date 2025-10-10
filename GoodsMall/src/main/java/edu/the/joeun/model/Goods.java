28e01df778be27bd7d1a547b9276d31a07ab6aa7
PHB-1994 <gudqls0991@naver.com>
2025-10-10 12:19:09 +0900

    goods select

---
+++ GoodsMall/src/main/java/edu/the/joeun/model/Goods.java
@@ -0,0 +1,22 @@
+package edu.the.joeun.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+// @Entity -> JPA 가 아니기 때문에 사용하지 않음
+public class Goods {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private int id;
+    // @Column(length=100) -> JPA 가 아니기 때문에 사용하지 않음
+    private String name;
+    private int price;
+    private int stock;
+}
