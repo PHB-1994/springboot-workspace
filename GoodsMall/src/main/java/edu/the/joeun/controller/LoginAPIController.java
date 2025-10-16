94ee345c97e00894dbda3ac2749e0bc9ca8b37c7
PHB-1994 <gudqls0991@naver.com>
2025-10-16 18:01:54 +0900

    intersecptor, session, login

--- GoodsMall/src/main/java/edu/the/joeun/controller/LoginAPIController.java
+++ GoodsMall/src/main/java/edu/the/joeun/controller/LoginAPIController.java
@@ -3,13 +3,14 @@ package edu.the.joeun.controller;
 import edu.the.joeun.service.UserService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class LoginAPIController {
 
     @Autowired
