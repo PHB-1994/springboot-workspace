c33dd29be957b9284b968bf134905aac84c96ba7
PHB-1994 <gudqls0991@naver.com>
2025-10-27 14:51:07 +0900

    board 게시물 상세 조회

--- GoodsCommunity/src/main/java/edu/thejoeun/main/controller/MainController.java
+++ GoodsCommunity/src/main/java/edu/thejoeun/main/controller/MainController.java
@@ -3,6 +3,8 @@ package edu.thejoeun.main.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 // api 주소와 .html 연결을 작성하는 공간
 @Controller
@@ -18,6 +20,17 @@ public class MainController {
 
     @GetMapping("/board")
     public String pageBoard(){
-        return "pages/board/boardList";
+        return "pages/board/boardList.html";
+    }
+
+    // 상세보기 페이지
+    // @GetMapping("/board/detail/{id} http://localhost:8080/board/detail/id=1
+    // 상세보기는 ? 쿼리형태의 http://localhost:8080/board/detail?id=1
+    // id 를 읽어서 게시물 조회
+    // @GetMapping("/board/detail/id={id}")
+    // public String pageBoardDetail(@PathVariable int id){
+    @GetMapping("/board/detail")
+    public String pageBoardDetail(@RequestParam int id){
+        return "pages/board/boardDetail.html";
     }
 }
--- GoodsCommunity/src/main/resources/static/js/board.js
+++ GoodsCommunity/src/main/resources/static/js/board.js
@@ -28,8 +28,13 @@ async function fetchBoardData() {
                 const row = document.createElement("tr")
 
                 row.innerHTML = `
-                <td class="title-cell" onclick="openModal(${b.id})">${b.title}</td>
+                <td class="title-cell">${b.id}</td>
+                <td>${b.title}</td>
                 <td>${b.writer}</td>
+                <td>${b.viewCount}</td>
+                <td>${b.createdAt}</td>
+                <td onclick="openModal(${b.id})">미리보기</td>
+                <td onclick="gotoDetail(${b.id})">상세보기</td>
                 `;
                 tbody.appendChild(row);
             }
@@ -40,6 +45,98 @@ async function fetchBoardData() {
 
 }
 
-// 페이지 로드 시 자동으로 특정 기능 실행
-window.addEventListener("DOMContentLoaded", fetchBoardData);
+// 게시글 상세보기를 호출하는 기능
+async function detailFunction(id) {
 
+    const res = await fetch(API_BASE_URL + `/board/${id}`);
+
+    if(!res.ok){
+        throw new Error("게시글을 불러오는데 실패했습니다.")
+    }
+
+    return await res.json(); // detailFunction 을 이용하면 백엔드에서 호출한 데이터가 반환
+}
+
+// 상세보기 페이지에서 실행할 기능
+async function fetchBoardDetail(id){
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const boardId = urlParams.get("id");
+
+    // 클라이언트가 DB 에 존재하지 않는 id 를 작성할 때 게시물 메인으로 돌려보내기
+    if(!boardId) {
+        alert("잘못된 게시글 번호입니다.")
+        window.location.href = "/board";
+    }
+
+    const b = await detailFunction(boardId);
+    console.log("DB 데이터 조회 : ", b);
+
+    const title = document.querySelector(".board-title");
+    const writer = document.querySelector(".board-writer");
+    const date = document.querySelector(".board-date");
+    const views = document.querySelector(".board-views");
+    const content = document.querySelector(".board-content");
+
+    title.textContent = b.title;
+    writer.textContent = b.writer;
+    date.textContent = b.createdAt;
+    views.textContent = b.viewCount;
+    content.textContent = b.content;
+}
+
+
+// 모달 열기 기능
+/**
+ * 모달에서 게시글 상세보기
+ * @param id id - 게시글 아이디
+ */
+async function openModal(id) {
+    const modal = document.getElementById("viewModal");
+    const modalTitle = document.getElementById("modalTitle");
+    const modalInfo = document.getElementById("modalInfo");
+    const modalContent = document.getElementById("modalContent");
+
+    modal.style.display = "flex";
+
+    // const res = await fetch(API_BASE_URL + `/board/${id}`);
+
+    const board = await detailFunction(id);
+
+    modalTitle.textContent = board.title;
+    modalInfo.textContent = `작성자 ${board.writer}`;
+    modalContent.textContent = board.content;
+
+}
+
+function closeModal() {
+    const modal = document.getElementById("viewModal");
+    modal.style.display = "none";
+}
+
+/**
+ * 페이지를 구분해서 함수 실행
+ */
+
+// 페이지 로드 시 자동으로 특정 기능 실행
+window.addEventListener("DOMContentLoaded", () => {
+    if(document.getElementById("boardTable")){
+        fetchBoardData();
+    }
+    if(document.querySelector(".board-detail-container")){
+        fetchBoardDetail();
+    }
+});
+
+const listBtn = document.querySelector(".btn-list");
+listBtn.addEventListener("click", gotoList);
+
+// 상세 페이지 이동 버튼 기능
+function gotoDetail(id){
+    window.location.href = `/board/detail?id=${id}`;
+}
+
+// 목록으로 돌아가기 버튼 기능
+function gotoList(){
+    window.location.href = `/board`;
+}
